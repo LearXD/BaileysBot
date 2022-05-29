e6068e2a94097b8d596eb53615bcd7016aa0e089
Lear XD <gutyxdcanal@gmail.com>
2022-05-28 22:23:51 -0300

    😊

--- src/start.ts
+++ src/start.ts
@@ -1,9 +1,5 @@
 import bot from "./bot";
 
-const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
-const ffmpeg = require('fluent-ffmpeg');
-ffmpeg.setFfmpegPath(ffmpegPath);
-
 console.log("INICIANDO O BOT");
 
 bot();
