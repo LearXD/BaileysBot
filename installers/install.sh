83db8986da0af99365b01a3df05f727f3b8d2912
LearXD <gutyxdcanal@gmail.com>
2022-06-10 17:35:35 -0300

    INSTALLERS FIX

--- installers/install.sh
+++ installers/install.sh
@@ -2,12 +2,9 @@
 
 sudo apt-get install -y npm libwebp wget
 
-cd ~
 curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh
 sudo bash nodesource_setup.sh
 sudo apt-get install nodejs -y
 sudo rm nodesource_setup.sh
-
-npm install
 
 echo "> Todas dependências foram instaladas, por favor inicie o bot usando: \"npm start\""
